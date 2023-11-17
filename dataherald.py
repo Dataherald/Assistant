aa30ba8b283dd97fec8a503c6c678848402df083
mohammadrezapourreza <m1378.prz@gmail.com>
2023-11-17 10:17:10 -0500

    Update dataherald host

--- dataherald.py
+++ dataherald.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-HOST = "http://streamlit_engine.dataherald.ai/api/v1/questions"
+HOST = "https://streamlit.dataherald.ai/api/v1/questions"
 DATABASES_IDS = {
     'RealEstate': '6537c3dc4cec532eccb7d6cc',
     'SenateStock': '65424c694cec532eccb7d766',
